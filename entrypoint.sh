b655bb91eb13ce59a826378309ddb1371cb4bd1d
furacas <s.furacas@outlook.com>
2023-09-27 22:38:03 +0800

    fix aceppt

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 warp-svc &
 sleep 2
 warp-cli --accept-tos register
-warp-cli set-proxy-port 40001
-warp-cli set-mode proxy
+warp-cli --accept-tos set-proxy-port 40001
+warp-cli --accept-tos set-mode proxy
 
 if [[ -n $WARP_LICENSE ]]; then
-  warp-cli set-license "${WARP_LICENSE}"
+  warp-cli --accept-tos set-license "${WARP_LICENSE}"
 fi
-warp-cli connect
+warp-cli --accept-tos connect
 
 socat tcp-listen:40000,reuseaddr,fork tcp:localhost:40001
